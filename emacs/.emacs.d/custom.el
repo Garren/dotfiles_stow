d462a69b76491227e676b8e41ebe1618179082ee
Adam Garren <garren@users.noreply.github.com>
2020-12-14 23:39:29 -0700

    add emacs

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fido-mode t)
+ '(next-screen-context-lines 5)
+ '(package-selected-packages
+   '(helm-slime whole-line-or-region helm ace-jump-mode undo-tree use-package zenburn-theme slime-company rainbow-delimiters paredit geiser color-theme-sanityinc-tomorrow))
+ '(slime-company-completion 'fuzzy))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
